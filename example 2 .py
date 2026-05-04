cc21967bb167e09a17d49540f4628e8ebd459bb8
sathana495 <senthilsathana447@gmail.com>
2026-05-04 14:57:26 +0530

    Add files via upload

---
+++ example 2 .py
@@ -0,0 +1,30 @@
+#get input from user
+'''
+a=input()
+b=int(input())
+print('my name is:',a)
+print('my age is:',b)'''
+
+'''a=input('enter ur name:')
+b=input('enter ur age:')
+c=input('enter ur address:')
+print('my name is:',a)
+print('my age is:',b)
+print('my address is:',c)'''
+
+#quiz 2 multiple and divided
+'''a=int(input())
+b=int(input())
+c=int(input())
+d=a*b*c
+e=a+b+c
+print(d//e)'''
+
+#quiz 3 score dived by 100
+name=input()
+department=input()
+score=int(input())
+
+print(' name',name)
+print('department',department)
+print('score',score/10,'/10')
