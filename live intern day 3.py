dbd6d90f3ca71df4e41dcc8a68da9df64e15f963
sathana495 <senthilsathana447@gmail.com>
2026-05-04 16:12:54 +0530

    Add files via upload

--- live intern day 3.py
+++ live intern day 3.py
@@ -1,12 +1,12 @@
 #control flow statements
 #conditional flow
-age = int(input('enter the your age:'))
+'''age = int(input('enter the your age:'))
 if age>18:
     print('you eligible for vote')
 elif age==18: 
     print('your eligible for vote')
 else:
-    print('your not eligible') 
+    print('your not eligible') '''
 
 
 '''age = int(input('enter the age:'))
@@ -33,7 +33,7 @@ else:
 '''for i in 'sathana':
     print(i)'''
 
-for i in range(5):
+'''for i in range(5):
     for j in range(5):
         print('*',end="")
     print()    
@@ -43,7 +43,86 @@ for i in range(5):
 for i in range(5):
     for j in range(i):
         print('*',end="")
-    print() 
+    print() '''
+# quiz 1
+'''n = int(input("Enter number of rows: "))
+
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        print(j, end=" ")
+    print()'''
+#quiz 2
+'''n = int(input("Enter n: "))
+
+for i in range(1, n+1):
+    print(f"Table of {i}")
+    for j in range(1, 11):
+        print(i, "x", j, "=", i*j)
+    print()'''
+
+
+'''for i in range(1,11):
+    print(i,'x','2','=',i*2)'''
+
+#quiz 3
+'''num=int(input('enter a number:'))
+if(num%2==0):
+    print('E')
+else:
+    print('O')'''
+
+#quiz 4
+'''n = int(input("Enter number: "))
+
+for i in range(2, n+1):
+    is_prime = True
+    for j in range(2, i):
+        if i % j == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(i, end=" ")'''
+
+#quiz 5
+'''marks = int(input('enter student mark:'))
+if marks >= 75:
+    print('Distinction')
+elif marks>= 50 and marks<=74:
+    print('pass')
+elif marks<50:
+    print('fail')
+else:
+    print('enter your correct mark') '''  
+
+#quiz 6
+n = int(input("Enter rows: "))
+m = int(input("Enter columns: "))
+
+print("Enter Matrix A:")
+A = []
+for i in range(n):
+    row = list(map(int, input().split()))
+    A.append(row)
+
+print("Enter Matrix B:")
+B = []
+for i in range(n):
+    row = list(map(int, input().split()))
+    B.append(row)
+
+# Addition
+result = []
+for i in range(n):
+    row = []
+    for j in range(m):
+        row.append(A[i][j] + B[i][j])
+    result.append(row)
+
+print("Result Matrix:")
+for row in result:
+    print(row)
+
+
 
 
 
