8001f67ed85e5a06ab2c63d4307b599c4443cdd4
sathana495 <senthilsathana447@gmail.com>
2026-06-05 21:30:25 +0530

    Add files via upload

---
+++ if else ex 1.py
@@ -0,0 +1,196 @@
+# quzii 1 pass or fail
+'''mark=int(input())
+if(mark>=35):
+    print("pass")
+else:
+    print("fail")    '''
+
+
+#get input income if income greater then 7000 print eligeble or not
+'''income=int(input())
+if(income>7000):
+    print('you are eligible fo scolarship')
+else:
+    print('you are not eligible') '''
+
+
+
+'''if(num%3==0 and num%5==0):
+    print('divisible by both 3 and 5')
+else:
+    print('not divisible by 3 and 5')  '''
+
+
+#get input and find even or odd
+'''num=int(input())
+if(num%2==0):
+    print('even')
+else:
+    print('odd')  '''
+
+
+#get score out of 100 find gud or avg or poor
+'''score=int(input())
+if(score<=35):
+    print('poor student')
+elif(score>35 and score<=70):
+    print('avg student')
+elif(score>=71 and score<=100):
+    print('gud student')    
+else:
+    print('error')'''
+
+
+#creat mini calculator
+'''a=int(input())
+b=int(input())
+operation=input('add/sub/div/mul')
+if(operation=="add"):
+    print(a+b)
+elif(operation=="sub"):
+    print(a-b)
+elif(operation=="mul"):
+    print(a*b)
+elif(operation=="div"):
+    print(a/b)
+else:
+    print("error")'''
+
+#vaarnam aayiram
+'''
+megna=input()
+if(megna=="died"):
+    print("surys meets priya")
+else:
+    print("surya weds megna")    
+
+    '''
+#qiuzz1(pass or fail)
+'''
+mark=int(input("enter the mark:"))
+if(mark>=35):
+    print("pass")
+else:
+    print("fail")    
+    '''
+
+#scolarship
+'''
+
+income=int(input("enter your incom:"))
+if(income<7000):
+    print("u are aligible for scolarship")
+else:
+    print("u are not aligible for scolarship")    
+    '''
+
+#divisable by 3 and 5
+'''a=int(input())
+if(a%3==0 and a%5==0):
+    print("yes")
+else:
+    print("no")    '''
+    # here the (and ^)is binary opretor
+
+#quiz what are do inside of the if
+'''score=int(input('enter the score:'))
+if(score>70):
+    name=input()
+    age=input()
+    departnent=input()
+    print('ur eligible for the scolarship')
+else:
+    print('ur not eligible')    '''
+
+
+#get input age and salary for loan amount
+'''salary=int(input('enter ur salary:'))
+age=int(input('enter ur age:'))
+if(salary>20000 or age<25):
+    print('u are required loan amount')
+    loan_amount=int(input('enter a amount for loan:'))
+    if(loan_amount<50000):
+        print('u are eligible for loan')
+    else:
+        print('u are not eligible for loan')
+else:
+    print('u are not required loan amount')    '''
+
+
+#get input for 5 subject mark
+'''tamil=int(input('entr a mark:'))
+enlish=int(input('enter a mark:'))
+maths=int(input('enter a mark:'))
+science=int(input('enter a mark'))
+social=int(input('enter a mark'))
+a=(tamil+enlish+maths+science+social)
+b=(a/5)
+print(b)
+if(b<35):
+    print('additional class is required')
+else:
+   print('u are gud to go')       '''
+
+# datatypes
+'''name="abi sana"
+age=18
+height=157.3
+weight=45
+print(name)
+print(age)
+print(height)
+print(type(name))
+print(type(age))'''
+
+
+
+ # operators
+'''a = int(input('enter a number'))
+b = int(input('enter a value'))
+#addition
+c = a+b
+print(c)
+#sub
+d = a-b
+print(d)
+#multiplication
+e = a*b
+print(e)
+#divid
+f = a//b
+print(f)'''
+
+#comparsion operator
+'''score=int(input('enter the score:'))
+if(score>70):
+    name=input('enter a name :')
+    age=input('enter a age :')
+    departnent=input('enter a department :')
+    print('ur eligible for the scolarship')
+else:
+    print('ur not eligible') '''
+
+# bitwise operator
+'''a=int(input("Enter the value a:"))
+b=int(input("Enter the value b:"))
+print(a^b)
+print(a>>b)
+print(a<<b)'''
+
+
+#identity operator
+fruits=["apple","banana","cherry"]
+print("apple" in fruits)
+print("orange" not in fruits)
+a=[1,2,3]
+b=[1,2,3]
+print(a==b)
+print(a is b)
+print(a is not b)
+
+
+
+
+    
+
+
