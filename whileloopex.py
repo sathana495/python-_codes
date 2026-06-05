8288ab0cf9546bc56ff2f05f36d5ce3bc73f1ea7
sathana495 <senthilsathana447@gmail.com>
2026-06-05 21:36:01 +0530

    Add files via upload

---
+++ whileloopex.py
@@ -0,0 +1,29 @@
+#example
+#whileloop is usually used to the number of iteration is unknown
+'''i=0
+while(i==0):
+    print('sathana')
+    i=1'''#put a condition in inside od a whileloop
+#quiz 1: print a number 1 to 10 using whileloop
+'''i=1
+while(i<5):
+    print(i)
+    i=i+1 '''
+#quiz 2:
+'''i=0
+while(i<200):
+    i=i+10
+    print(i,end=",")
+   #here,end="" is used to print a output in one line'''
+#quiz 3:
+'''i=10
+while(i>0):
+    print(i,end=',')
+    i=i-1'''
+#quiz 4:! question
+'''i=3
+fact=1
+while(i>0):
+    fact=fact*i
+    i=i-1
+print(fact)  '''  
