7dd5506166b17e0b056e3aeb812a5c10927f6792
sathana495 <senthilsathana447@gmail.com>
2026-06-05 21:29:47 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,86 @@
+#quzii 1
+'''def add():
+    a=int(input())
+    b=int(input())
+    print(a+b)
+
+def sub():
+    c=int(input())
+    d=int(input())
+    print(c-d)
+
+add()    
+sub()'''
+#quzii parameter fucntion
+'''def insta(msg):
+    print("massage:",msg)
+
+insta("hello...") '''   
+#quzii 3 find odd or even by using passing
+'''def oddoreven(a):
+    if(a%2==0):
+        print("even")
+    else:
+        print("odd") 
+
+z=int(input())
+oddoreven(z)  '''
+#quzi 4 pass or fail
+'''def passorfail(mark):
+    if(mark>35):
+        print("pass")
+    else:
+        print("fail")
+b=int(input())
+passorfail(b) '''
+#quzii 5 using by range
+'''def printrange(a,b):
+    for i in range(a,b): 
+        print(i)
+    
+c=int(input())
+d=int(input())    
+printrange(c,d+1)'''
+
+#quzii 6 using by returnkeyword
+'''def weekend(msg):
+    return msg
+word="happy.."
+print(weekend(word))'''
+
+#get user name and password and check it is same or not(using return key function)
+user_name='sana'
+pass_word='52006'
+
+username=input()
+password=input()
+
+def validate():
+    if(username==user_name and password==pass_word):
+       return"same"
+    else:
+        return"not same"
+    
+a=validate()
+print(a)
+
+   
+
+#quzii 8 print the output of (a+b)*c
+'''def add(a,b):
+    return  a+b
+c=int(input())
+d=int(input())
+e=int(input())
+added=add(c,d)
+output=added*e
+print(output)'''
+
+#quzii 9 
+
+
+
+
+
+
+
