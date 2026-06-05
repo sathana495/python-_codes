00cb5d6f8c1d21dd42b2527bf63a959e9cab7e32
sathana495 <senthilsathana447@gmail.com>
2026-06-05 21:34:45 +0530

    Add files via upload

---
+++ logical op.py
@@ -0,0 +1,13 @@
+'''
+a=10
+b=5
+statement1=(a==10)
+statement2=(b==10)
+print(statement1 or statement2)
+
+'''
+#quiz
+num=10
+print(num>5 and num<=10)
+print(num%2==0 or num%3==0)
+print(not(num==0))
